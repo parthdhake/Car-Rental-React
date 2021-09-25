8b5f949870aa66fda8288f15353ddcf36258480f
parthdhake <parthdhake@gmail.com>
2021-09-25 23:23:45 +0530

    filters added

--- src/pages/Cars.js
+++ src/pages/Cars.js
@@ -6,14 +6,33 @@ import VehicleList from "../components/VehicleList";
 import Select from "react-select";
 import { Button } from "@mui/material";
 
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
+const typeOptions = [
+  { value: "coupe", label: "Coupe" },
+  { value: "sedan", label: "Sedan" },
+  { value: "truck", label: "Truck" },
+  { value: "suv", label: "SUV" },
+  { value: "mini van", label: "Mini-Van" },
+  { value: "convertable", label: "Convertable" },
+];
+
+const seatsOptions = [
+  { value: 2, label: "2 Seats" },
+  { value: 4, label: "4 Seats" },
+  { value: 5, label: "5 Seats" },
+  { value: 7, label: "7 Seats" },
+  { value: 8, label: "8 Seats" },
+];
+
+const priceOptions = [
+  { value: "40-60", label: "40$ - 60$" },
+  { value: "61-80", label: "61$ - 80$" },
+  { value: "81-100", label: "81$ - 100$" },
 ];
 
 export default function Cars() {
   const [cars, setCars] = React.useState([]);
+
+  const [params, setParams] = React.useState({});
 
   useEffect(() => {
     axios.get("http://localhost:5000/cars").then((res) => {
@@ -22,18 +41,79 @@ export default function Cars() {
     });
   }, []);
 
+  const filterCars = () => {
+    console.log(params);
+    axios
+      .get("http://localhost:5000/cars", {
+        params: params,
+      })
+      .then((res) => {
+        setCars(res.data);
+        console.log(res.data);
+      });
+  };
+
+  const handleTypeChange = (data) => {
+    if (data == null) {
+      setParams({ ...params, type: null });
+      return;
+    }
+    setParams({
+      ...params,
+      type: data.value,
+    });
+  };
+
+  const handleSeatChange = (data) => {
+    if (data == null) {
+      setParams({ ...params, seats: null });
+      return;
+    }
+    setParams({
+      ...params,
+      seats: data.value,
+    });
+  };
+
+  const handlePriceChange = (data) => {
+    if (data == null) {
+      setParams({ ...params, price: null });
+      return;
+    }
+    setParams({
+      ...params,
+      price: data.value,
+    });
+  };
+
   return (
     <div>
       <Header pageName="Cars" />
       <div className="container" style={{ zIndex: 2, marginTop: "40px" }}>
         <div className="filter">
-          <Select options={options} />
-          <Select options={options} />
-          <Select options={options} />
+          <Select
+            name="type"
+            onChange={handleTypeChange.bind(this)}
+            isClearable={true}
+            options={typeOptions}
+          />
+          <Select
+            name="seat"
+            onChange={handleSeatChange.bind(this)}
+            isClearable={true}
+            options={seatsOptions}
+          />
+          <Select
+            name="price"
+            onChange={handlePriceChange.bind(this)}
+            isClearable={true}
+            options={priceOptions}
+          />
         </div>
         <div className="filter">
           <Button
             variant="contained"
+            onClick={() => filterCars()}
             color="success"
             style={{ marginTop: "20px" }}
           >
